bb33ab1fa963d9aadcb21ef6f431b03773377f75
adityaraj1682 <107756134+adityaraj1682@users.noreply.github.com>
2026-06-04 01:22:21 +0530

    email changes4

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -203,8 +203,8 @@ export async function login(req, res) {
             res.cookie('jwt', token, {
                 maxAge: 7 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: "strict",
-                secure: process.env.NODE_ENV === "production"
+                sameSite: "none",
+                secure: true
             });
 
             return res.status(200).json({
